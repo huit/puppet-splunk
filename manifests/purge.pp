5cfe704bbae8c218c9eee027b8b9b1a2a779a641
Tim Hartmann <tfhartmann@gmail.com>
2013-06-03 16:24:08 -0400

    Added splunk::purge
    Purge will get rid of everything SPLUNK on your system. If you have an
    install and want to get rid if it, and any splunk universal forwarder, use purge. It
    also deletes everything connected to the Splunk RPM, so if you've built your own custom
    RPMs to deploy TA's, it purges the heck out of them

---
+++ manifests/purge.pp
@@ -0,0 +1,28 @@
+# USE WITH CAUTION
+# splunk::purge takes no parameters and is dangerous!
+# This class will PURGE all Splunk from your system. It's not really great
+# To keep this in your node defintions as package for us requred purge, since
+# we installed TA's with RPM's and so need package - purged to ensure depencies
+# got removed
+class splunk::purge () {
+  service {
+    'splunk':
+      ensure     => stopped,
+      enable     => false,
+      hasrestart => true,
+      pattern    => 'splunkd',
+      before     => Package['splunk','splunkforwarder'],
+  }
+  package { ['splunk','splunkforwarder']:
+    ensure   => purged,
+  } ->
+
+  file { '/etc/init.d/splunk':
+    ensure => absent,
+  }
+  file { ['/opt/splunk','/opt/splunkforwarder']:
+    ensure  => absent,
+    force   => true,
+    recurse => true,
+  }
+}
