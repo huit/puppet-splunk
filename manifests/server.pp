e9bea595f5c0d3f3873f7d318878914976722aff
Tim Hartmann <tfhartmann@gmail.com>
2013-06-14 15:53:26 -0400

    Deleted server.pp

--- manifests/server.pp
+++
@@ -1,9 +0,0 @@
-class splunk::server (
-) {
-  $pyversion = $::lsbmajdistrelease ? {
-    5 => '/usr/lib/python2.4',
-    6 => '/usr/lib/python2.6',
-  } 
-  file { '/usr/lib/python': ensure => "$pyversion" }
-  
-}   
