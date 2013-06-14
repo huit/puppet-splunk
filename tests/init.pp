916a595187018529585306a1b2a53f3a3627e887
Tim Hartmann <tfhartmann@gmail.com>
2013-06-14 17:08:07 -0400

    Adding updated tests

--- tests/init.pp
+++ tests/init.pp
@@ -1,4 +1,5 @@
 class { 'splunk':
   port         => '50514',
-  target_group => { 'name' => '1.2.3.4' },
+  target_group => { 'name' => 'splunkindex-60ox.noc.harvard.edu' },
 }
+splunk::ta::files { 'Splunk_TA_nix': }
