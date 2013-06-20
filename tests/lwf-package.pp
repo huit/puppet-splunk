27b35b1c19e9d1fac8a8b395c49328928fe8d456
Tim Hartmann <tfhartmann@gmail.com>
2013-06-20 12:01:04 -0400

    Added LWF Test for package ta

---
+++ tests/lwf-package.pp
@@ -0,0 +1,25 @@
+class { 'splunk':
+  index        => 'ns-os',
+  type         => 'lwf',
+  port         => '50514',
+  target_group => { 'name' => 'splunkindex-60ox.noc.harvard.edu' },
+}
+splunk::ta::package { 'unix':
+  inputfile => 'splunk/Splunk_TA_nix/inputs.conf.erb',
+}
+class { 'splunk::inputs': }
+#class { 'splunk::inputs': 
+#  input_hash   => { 'script://./bin/sshdChecker.sh' => {
+#                       disabled   => 'true',
+#                       index      => 'os',
+#                       interval   => '3600',
+#                       source     => 'Unix:SSHDConfig',
+#                       sourcetype => 'Unix:SSHDConfig'},
+#                     'script://./bin/sshdChecker.sh2' => {
+#                       disabled   => 'true2',
+#                       index      => 'os2',
+#                       interval   => '36002',
+#                       source     => 'Unix:SSHDConfig2',
+#                       sourcetype => 'Unix:SSHDConfig2'}
+#                   }
+#}
