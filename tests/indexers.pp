709b0fa66f836e0db944442344ece5c1ebce571a
Tim Hartmann <tfhartmann@gmail.com>
2013-07-18 17:25:31 -0400

    Adding transforms.conf and props.conf

--- tests/indexers.pp
+++ tests/indexers.pp
@@ -1,20 +1,32 @@
 class { 'splunk':
   type => 'indexer',
-  output_hash => { 'syslog:qradar_group' => {
-                     'server' => 'q6.itsec.harvard.edu:514' }
+  output_hash => { 'syslog:qradar_group' => 
+                     { 'server' => 'q6.itsec.harvard.edu:514' }
                  }
 }
 class { 'splunk::inputs':
   input_hash =>  { 'splunktcp://50514' => {} }
-  }
+}
+class { 'splunk::props':
+  input_hash => {
+                  'lsof' => 
+                    { 'TRANSFORMS-null' => 'setnull' },
+                  'ps' => 
+                    { 'TRANSFORMS-null' => 'setnull' },
+                  'linux_secure' =>
+                    { 'TRANSFORMS-nyc'  => 'send_to_qradar' }
+                }
+}
+class { 'splunk::transforms':
+  input_hash => {
+                  'setnull' => 
+                    { 'REGEX'    => '.',
+                      'DEST_KEY' => 'queue',
+                      'FORMAT'   => 'nullQueue' },
+                  'send_to_qradar' =>
+                    { 'REGEX'    => '.',
+                      'DEST_KEY' => '_SYSLOG_ROUTING',
+                      'FORMAT'   => 'qradar_group' }
+                }
+}
 splunk::ta::files { 'Splunk_TA_nix': }
-
-#class { 'splunk::indexes': 
-#  input_hash => { 'dns' => {
-#                    'homePath'               => '$SPLUNK_DB/dns/db',
-#                    'coldPath'               => '/mnt/splunk/dns/colddb',
-#                    'thawedPath'             => '$SPLUNK_DB/dns/thaweddb',
-#                    'homePath.maxDataSizeMB' => '102400',
-#                    'frozenTimePeriodInSecs' => '3888000' }
-#                }
-#}
